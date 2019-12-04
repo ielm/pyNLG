e78913417ea8bbcd54d5103221e0628199e221e3
ielm <leoni@rpi.edu>
2019-12-03 19:39:42 -0500

    implement LexicalCategory

---
+++ nlg/framework/LexicalCategory.py
@@ -0,0 +1,32 @@
+from nlg.framework.ElementCategory import ElementCategory
+
+from enum import Enum
+
+class LexicalCategory(ElementCategory):
+
+    class LexicalCategoryEnum(Enum):
+        ANY             = "ANY"
+        SYMBOL          = "SYMBOL"
+        NOUN            = "NOUN"
+        ADJECTIVE       = "ADJECTIVE"
+        ADVERB          = "ADVERB"
+        VERB            = "VERB"
+        DETERMINER      = "DETERMINER"
+        PRONOUN         = "PRONOUN"
+        CONJUNCTION     = "CONJUNCTION"
+        PREPOSITION     = "PREPOSITION"
+        COMPLEMENTISER  = "COMPLEMENTISER"
+        MODAL           = "MODAL"
+        AUXILIARY       = "AUXILIARY"
+
+    def __init__(self, lexical_category: LexicalCategoryEnum):
+        self._lexicalCategory = lexical_category
+
+    def __eq__(self, other):
+        match = False
+        if other is not None:
+            if isinstance(other, LexicalCategory):
+                match = True
+            else:
+                match = self._lexicalCategory == str(other)
+        return match
